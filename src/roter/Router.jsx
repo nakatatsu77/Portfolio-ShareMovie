772edecc9d23320043c91e86aa2f4e6c879744c8
nakatatsu77 <nakatatsu1177@gmail.com>
2022-10-20 17:53:20 +0900

    ルーティングの設定

---
+++ src/roter/Router.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Login } from "../pages/Login";
+import { Page404 } from "../pages/Page404";
+import { Setting } from "../pages/Setting";
+import {SignUp} from "../pages/SignUp";
+import { SharePage } from "../pages/SharePage";
+
+export const Router = () => {
+  return (
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/setting" element={<Setting />} />
+    <Route path="/share" element={<SharePage />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/signup" element={<SignUp />} />
+
+    <Route path="/*" element={<Page404 />} />
+  </Routes>
+  )
+}
