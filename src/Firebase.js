58488831e72962d4d03060919a4f82a585c05bc5
nakatatsu77 <nakatatsu1177@gmail.com>
2022-10-20 17:55:20 +0900

    Firebaseのインストール

---
+++ src/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCIdl8q5UQUSASqJ6CUbmbnWxsCH3_kku8',
+  authDomain: 'react-portfolio-dca7d.firebaseapp.com',
+  projectId: 'react-portfolio-dca7d',
+  storageBucket: 'react-portfolio-dca7d.appspot.com',
+  messagingSenderId: '963883252715',
+  appId: '1:963883252715:web:90dc75080c55c1247cf7ae',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
