4f874da17ed5c6286303fd4e26d1f44194218cba
nakatatsu77 <106891112+nakatatsu77@users.noreply.github.com>
2022-11-10 21:35:39 +0900

    Revert "update: データを格納できるようにした"

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Flex, Heading, Img, Stack, Text } from "@chakra-ui/react";
+import firebase from "firebase/compat/app";
 import { db, auth } from "../../Firebase.js";
 import {
+  addDoc,
   collection,
   doc,
-  setDoc
+  getDocs,
+  onSnapshot,
+  setDoc,
 } from "firebase/firestore";
 
 const API_IMG = "https://image.tmdb.org/t/p/w154/";
@@ -13,27 +17,36 @@ export const Movie = ({ title, poster_path, release_date, id }) => {
   const { uid } = auth.currentUser;
 
   const handleClickAddButton = async () => {
+    //データベースにクリックした映画データの追加
     try {
-      const addData = { title, id, release_date, poster_path, API_IMG, uid, };
-      const docRef = doc(collection(db, "favoriteMovies"));
-      await setDoc(docRef, addData);
+      const docRef = await addDoc(collection(db, "favoriteMovies"), {
+        title,
+        id,
+        release_date,
+        poster_path,
+        API_IMG,
+        uid,
+      });
+      console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
 
   return (
-    <Flex borderRadius="10px" bg="white" p="2px" mb={3} shadow="lg">
-      <Img src={API_IMG + poster_path} />
-      <Heading as="h1" fontSize="lg" ml={3} mt={3}>
-        <Stack spacing={5}>
-          <Text>{title}</Text>
-          <Text>{release_date}公開</Text>
-        </Stack>
-        <Button onClick={handleClickAddButton} colorScheme="twitter" mt={5}>
-          みんなとシェア！
-        </Button>
-      </Heading>
-    </Flex>
+    <>
+      <Flex borderRadius="10px" bg="white" p="2px" mb={3} shadow="lg">
+        <Img src={API_IMG + poster_path} />
+        <Heading as="h1" fontSize="lg" ml={3} mt={3}>
+          <Stack spacing={5}>
+            <Text>{title}</Text>
+            <Text>{release_date}公開</Text>
+          </Stack>
+          <Button onClick={handleClickAddButton} colorScheme="twitter" mt={5}>
+            みんなとシェア！
+          </Button>
+        </Heading>
+      </Flex>
+    </>
   );
 };
